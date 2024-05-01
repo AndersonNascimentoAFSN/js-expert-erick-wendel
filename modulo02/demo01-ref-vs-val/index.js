6c0c614d9a359e8f871842702326ebbcefce65ea
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-04-30 22:55:02 -0300

    feat: imutabilidade

---
+++ modulo02/demo01-ref-vs-val/index.js
@@ -0,0 +1,26 @@
+'strict'
+
+const { deepStrictEqual } = require('assert')
+
+let counter = 0
+let counter2 = counter
+
+counter2++
+
+const item = { counter: 0 }
+const item2 = item
+
+item2.counter++
+
+{
+  // tipo primitivo gera uma cópia em memória
+  deepStrictEqual(counter, 0)
+  deepStrictEqual(counter2, 1)
+}
+
+{
+  // tipo de referência, aponta para o mesmo local em memória
+  deepStrictEqual(item, { counter: 1 })
+  deepStrictEqual(item2, { counter: 1 })
+}
+
